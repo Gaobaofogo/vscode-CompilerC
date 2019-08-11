5457c538271669be225f5f8025098cc90c1ea83e
gabrieldiniz54 <gabrieldiniz54@gmail.com>
2019-08-11 00:39:56 -0300

    Code commented

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 
+		// Provides an interface to the window the user is currently in
 		let editor = vscode.window.activeTextEditor;
 
+		// If the window is open (check required)
 		if (editor) {
 
-			const file_name = editor.document.fileName;
-			const compile_file = file_name.slice(0, -2);
+			const file_name = editor.document.fileName; // Absolute path of the file that is open
+			const compile_file = file_name.slice(0, -2); // Absolute path of the compiled file to be created
 			
-			const terminal = vscode.window.createTerminal();
-			terminal.sendText(`gcc ${file_name} -o ${compile_file}`);
-			terminal.sendText('clear');
-			terminal.sendText(`${compile_file}`);
-			terminal.show();
+			const terminal = vscode.window.createTerminal(); // Create as integrated terminal
+			terminal.sendText(`gcc ${file_name} -o ${compile_file}`); // Send command 'gcc file.c -o file'
+			terminal.sendText('clear'); // Send command 'clear' only for aesthetic reasons
+			terminal.sendText(`${compile_file}`); // Execute the compiled file
+			terminal.show(); // Show the terminal on screen
 		}
 
 	});
